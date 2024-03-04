8ddb38f29a9ac94266798b222eca1ae9f18322be
Nima Fotouhi <nimaft@amazon.com>
2024-03-04 10:11:44 -0800

    feat: added AL2023 support
    Added support for AL2023 EKS-Optimized AMIs
    NodeGroupInfoLambda was updated to create different user data for AL2 and AL23 due to different mechanism used by AL23 to join the cluster (nodeadm)
    Updated lambda's permissions to read stack details to determine whether AL23 or AL2 is being used and create user data accordingly
    
    Signed-off-by: Nima Fotouhi <nimaft@amazon.com>

--- functions/node_group_info.py
+++ functions/node_group_info.py
@@ -1,4 +1,5 @@
 import traceback
+import os
 import base64
 import textwrap
 from aws_lambda_powertools import Logger
@@ -6,18 +7,14 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 import boto3
 from botocore.config import Config
 from botocore.exceptions import BotoCoreError, ClientError
-
 # Configure logging
 logger = Logger(service="Launch template updater", level="INFO")
 # Configure Boto3 retry mode with max_attempts
 boto3_config = Config(retries={"mode": "standard", "max_attempts": 10})
-
-
 def handle_errors(func):
     """
     A decorator to handle exceptions that might be raised by the wrapped function.
     """
-
     @logger.inject_lambda_context
     def wrapper(*args, **kwargs):
         try:
@@ -52,10 +49,7 @@ def handle_errors(func):
                 "status": "error",
                 "message": f"An unexpected error occurred: {str(e)}",
             }
-
     return wrapper
-
-
 def list_clusters(eks_client):
     """
     Lists all EKS clusters.
@@ -69,8 +63,6 @@ def list_clusters(eks_client):
     for page in paginator.paginate():
         clusters.extend(page["clusters"])
     return clusters
-
-
 def filter_clusters(eks_client, clusters, required_tags):
     """
     Retrieves node groups for clusters with the required tags.
@@ -84,13 +76,11 @@ def filter_clusters(eks_client, clusters, required_tags):
     required_tags_set = (
         {(tag["Key"], tag["Value"]) for tag in required_tags} if required_tags else None
     )
-
     def has_required_tags(cluster_tags):
         if not required_tags_set:
             return True
         cluster_tags_set = {(tag["key"], tag["value"]) for tag in cluster_tags}
         return required_tags_set.issubset(cluster_tags_set)
-
     filtered_clusters = []
     for cluster in clusters:
         cluster_summary = {}
@@ -104,11 +94,10 @@ def filter_clusters(eks_client, clusters, required_tags):
                 "cluster-ca": cluster["certificateAuthority"].get("data"),
                 "endpoint": cluster["endpoint"],
                 "dns": ".".join(raw_dns.split("/")[0].split(".")[:-1] + ["10"]),
+                "cidr": raw_dns,
             }
             filtered_clusters.append(cluster_summary)
     return filtered_clusters
-
-
 def get_node_groups(eks_client, filtered_clusters):
     """
     Gets the node groups associated with the cluster.
@@ -141,10 +130,35 @@ def get_node_groups(eks_client, filtered_clusters):
                     "cluster-ca": cluster["cluster-ca"],
                     "endpoint": cluster["endpoint"],
                     "dns": cluster["dns"],
+                    "cidr": cluster["cidr"],
                 }
                 node_groups.append(node_group)
     return node_groups
 
+def get_parent_image_info(region, stack_name):
+    """
+    Returns the current StackVersion from the CloudFormation stack output.
+    """
+    cloudformation = boto3.client("cloudformation", region_name=region)
+    stacks = []
+    next_token = None
+    while True:
+        if next_token:
+            response = cloudformation.describe_stacks(
+                StackName=stack_name, NextToken=next_token
+            )
+        else:
+            response = cloudformation.describe_stacks(StackName=stack_name)
+        stacks.extend(response["Stacks"])
+        next_token = response.get("NextToken")
+        if not next_token:
+            break
+    stack_parameters = response["Stacks"][0]["Parameters"]
+    latest_eks_optimized_ami = None
+    for parameter in stack_parameters:
+        if parameter["ParameterKey"] == "LatestEKSOptimizedAMI":
+            latest_eks_optimized_ami = parameter["ParameterValue"]
+    return latest_eks_optimized_ami
 
 def update_launch_templates(node_groups, image_id):
     """
@@ -157,27 +171,47 @@ def update_launch_templates(node_groups, image_id):
         A list of dictionaries containing the updated node group information.
     """
     ec2_client = boto3.client("ec2")
+    region = os.environ.get("REGION", "")
+    stack_name = os.environ.get("STACK_NAME", "")
+    ssm_parameter_name = get_parent_image_info(region, stack_name)
     processed_launch_templates = set()
     for node_group in node_groups:
         launch_template = node_group["launchTemplate"]
         launch_template_id = launch_template["id"]
         launch_template_version = launch_template["version"]
-        user_data = textwrap.dedent(
-            f"""
-        MIME-Version: 1.0
-        Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
-        --==MYBOUNDARY==
-        Content-Type: text/x-shellscript; charset="us-ascii"
-        #!/bin/bash
-        set -ex
-        /etc/eks/bootstrap.sh {node_group["clusterName"]} \\
-            --b64-cluster-ca {node_group["cluster-ca"]} \\
-            --apiserver-endpoint {node_group["endpoint"]} \\
-            --dns-cluster-ip {node_group["dns"]} \\
-            --container-runtime containerd
-        --==MYBOUNDARY==--
-        """
-        ).strip()
+        if "amazon-linux-2023" in ssm_parameter_name:
+            user_data = textwrap.dedent(
+                f"""
+                ---
+                apiVersion: node.eks.aws/v1alpha1
+                kind: NodeConfig
+                spec:
+                    cluster:
+                        name: {node_group["clusterName"]}
+                        apiServerEndpoint: {node_group["endpoint"]}
+                        certificateAuthority: {node_group["cluster-ca"]}
+                        cidr: {node_group["cidr"]}
+                """
+                ).strip()
+        elif "amazon-linux-2" in ssm_parameter_name:
+            user_data = textwrap.dedent(
+                f"""
+                MIME-Version: 1.0
+                Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
+                --==MYBOUNDARY==
+                Content-Type: text/x-shellscript; charset="us-ascii"
+                #!/bin/bash
+                set -ex
+                /etc/eks/bootstrap.sh {node_group["clusterName"]} \\
+                    --b64-cluster-ca {node_group["cluster-ca"]} \\
+                    --apiserver-endpoint {node_group["endpoint"]} \\
+                    --dns-cluster-ip {node_group["dns"]} \\
+                    --container-runtime containerd
+                --==MYBOUNDARY==--
+                """
+                ).strip()
+        else:
+            logger.error("Invalid parameter value for base AMI")
         encoded_user_data = base64.b64encode(user_data.encode("utf-8")).decode("utf-8")
         if launch_template_id not in processed_launch_templates:
             new_launch_template_response = ec2_client.create_launch_template_version(
@@ -202,8 +236,6 @@ def update_launch_templates(node_groups, image_id):
             )
             node_group["version"] = latest_version_number
     return node_groups
-
-
 @handle_errors
 def lambda_handler(event, context):
     """
